92747fe63afa910930c685225f171847758de333
Nicco <Nicco@192.168.0.11>
2013-02-27 04:38:20 -0500

    fix the fucking garbage git problems

--- app/views/Rules/list.blade.php
+++ app/views/Rules/list.blade.php
@@ -1,7 +1,13 @@
 @section('content')
 <h1>STARLEAGUE RULES</h1>
 <h3>A COMPREHENSIVE LIST OF THE RULES OF STARLEAGUE</h3>
-
+@foreach($rules as $rule)
+<div class="indent-{{$rule->dots}}">
+  <h4>{{$rule->rule}}</h4>
+  <h4>{{$rule->title}}</h4>
+  <div>{{$rule->text}}</div>
+</div>
+@endforeach
 
 
 
