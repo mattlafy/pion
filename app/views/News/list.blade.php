a5fb7f0f778666735b08494df51fa76fbad58da4
Nicco <Nicco@192.168.0.11>
2013-02-28 02:40:35 -0500

    fix the fucking garbage git problems

--- app/views/News/list.blade.php
+++ app/views/News/list.blade.php
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-@section('content')
-<h1>STARLEAGUE NEWS</h1>
-<h3>THE LATEST STARLEAGUE NEWS UPDATES</h3>
-
-@foreach($news_posts as $temp)
-<h4>{{$temp->title}}</h4>
-<div>{{$temp->content}}</div>
-<div> Written by {{$temp->author}} on {{$temp->date_posted}} </div>
-@endforeach
-=======
 @section('content')
 <h1>STARLEAGUE NEWS</h1>
 <h3>THE LATEST STARLEAGUE NEWS UPDATES</h3>
@@ -23,4 +12,3 @@
 @yield('content')
 
 @stop
->>>>>>> b82aa52030a67b6dd951eb829dd2896660da1661
